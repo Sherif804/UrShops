1f2d97906d72f8bae81ef9951672e36654d7b3ed
Sherif Ahmed <sheriff.ahmedd.2001@gmail.com>
2021-12-25 18:47:01 +0200

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -96,8 +96,42 @@ let isSuperAdminAuthorized = async (req, res, next) => {
     }
 }
 
+let isSharedAuthorized = async (req, res, next) => {
+    try {
+        let response;
+        if (req.headers.authorization) {
+            let bareToken = req.headers.authorization;
+            let token = bareToken.split(" ")[1];
+            let decoded = decode(token);
+            let decodedArrayLength = decoded.split(process.env.SECRET_AUTH_KEY).length;
+            let lastDecodedElement = decoded.split(process.env.SECRET_AUTH_KEY)[3];
+            req.role = decoded.split(process.env.SECRET_AUTH_KEY)[2];
+            req.id = decoded.split(process.env.SECRET_AUTH_KEY)[1];
+            if( ( req.role == "admin" || req.role == "superAdmin" || req.role == "user") && decodedArrayLength == 4 && lastDecodedElement == process.env.SECRET_HASH_KEY) {
+                next(req,res);
+            }
+            else {
+                response = { status: 401, message: "unauthorized" };
+                response = JSON.stringify(response);
+                res.end(response);
+            }
+        }
+        else {
+            response = { status: 401, message: "unauthorized" };
+            response = JSON.stringify(response);
+            res.end(response);
+        }
+    } catch (err) {
+        response = { status: 500, message: "Something went wrong"};
+        response = JSON.stringify(response);
+        res.end(response);
+    }
+}
+
+
 module.exports = {
     isUserAuthorized,
     isAdminAuthorized,
-    isSuperAdminAuthorized
+    isSuperAdminAuthorized,
+    isSharedAuthorized
 }
